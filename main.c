3bfcd338988b2926426f37b70b305d83833c64e5
fairbrook <kevinvr@hotmail.es>
2020-09-15 08:59:46 -0500

    leer edad

--- main.c
+++ main.c
@@ -2,10 +2,14 @@
 
 int main()
 {
+    unsigned int edad;
     char nombre[100];
     printf("Hola mundo\n");
     printf("Nombre: ");
     fgets(nombre, sizeof(nombre), stdin);
     printf("Hola %s", nombre);
+    printf("Edad: ");
+    scanf("%u", &edad);
+    printf("Vas a cumplir %u", edad + 1);
     return 0;
 }
