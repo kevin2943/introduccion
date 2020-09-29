e6aadff26c5f4b9dd3e5e902f85d9ef59bd6ee9a
fairbrook <kevinvr@hotmail.es>
2020-09-29 12:02:18 -0500

    prueba1

--- funciones.h
+++ funciones.h
@@ -1,12 +1,18 @@
 #ifndef FUNCTIONES_H
 #define FUNCTIONES_H
 
-int suma(int a, int b){
-    return a+b;
+int suma(int a, int b)
+{
+    return a + b;
 }
 
-int resta( int a, int b){
-    return a-b;
+int resta(int a, int b)
+{
+    if (a < 0)
+    {
+        return a;
+    }
+    return a - b;
 }
 
 #endif
